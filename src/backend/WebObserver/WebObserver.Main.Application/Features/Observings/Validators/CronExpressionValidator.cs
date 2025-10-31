00a7c61f818c3d69f36bf2a8863c2eb2efb73659
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2025-10-31 22:54:12 +0300

    добавил ограничение для крона: максимум два раза в день

--- src/backend/WebObserver/WebObserver.Main.Application/Features/Observings/Validators/CronExpressionValidator.cs
+++ src/backend/WebObserver/WebObserver.Main.Application/Features/Observings/Validators/CronExpressionValidator.cs
@@ -14,5 +14,17 @@ public class CronExpressionValidator : AbstractValidator<string?>
         RuleFor(x => x)
             .Must(str => CronExpression.TryParse(str, out _))
             .WithMessage("CronExpression is invalid");
+        
+        RuleFor(x => x)
+            .Must(str =>
+            {
+                var expr = CronExpression.Parse(str);
+                var occurrences = expr
+                    .GetOccurrences(DateTime.UtcNow.Date, DateTime.UtcNow.Date.AddDays(1))
+                    .Count();
+
+                return occurrences <= 2; 
+            })
+            .WithMessage("CronExpression can occur at most two times a day");
     }
 }
